8a20180a3b77ac3f8b8f7cdeb5204687da9a285f
Faizan Ali <80191968+ycb-python@users.noreply.github.com>
2023-07-24 15:11:03 +0500

    Update copart_api.py

--- copart_api.py
+++ copart_api.py
@@ -11,7 +11,7 @@ from selenium import webdriver
 
 def get_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome(options=chrome_options)
